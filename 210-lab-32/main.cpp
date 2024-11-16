d8fc67acf221f04ebc1e6ccd903a2e6b3dc54b95
Errant27 <temporal.cerauno@gmail.com>
2024-11-15 18:10:34 -0800

    commit 3

--- 210-lab-32/main.cpp
+++ 210-lab-32/main.cpp
@@ -6,31 +6,56 @@
 using namespace std;
 
 const int SIZE = 2;
+const int WIDTH = 3;
+int rand_num();
 
 int main() {
     deque<Car> tollBooth(SIZE);
     
-    cout << "Intial deque:" << endl;
-    for (auto i = 0; i < tollBooth.size(); i++) {    // Outputs the inital queue
-        cout<< "[" << tollBooth.at(i).getYear() << " " << tollBooth.at(i).getMake();
-        cout<< " (" <<  tollBooth.at(i).getTransponder() << ")]"<< endl;
+    cout << "Intial queue:" << endl;
+    for (auto i = 0; i < tollBooth.size(); i++) {    // Outputs the inital queue of the deque
+        cout << setw(WIDTH) << "[" << tollBooth.at(i).getYear() << " " << tollBooth.at(i).getMake();
+        cout << " (" <<  tollBooth.at(i).getTransponder() << ")]"<< endl;
     }
     cout << "---------\n";
     
-    Car car1;
-    tollBooth.push_back(car1);
-    Car car2;
-    tollBooth.push_back(car2);
-    Car car3;
-    tollBooth.push_back(car3);
-    Car car4;
-    tollBooth.push_back(car4);
+    static int count = 1;
+    while (tollBooth.empty() == false) {
+        cout << "Time: " << count << endl;
+        int oper = rand_num();
+        
+        
+        if (oper <= 45) {
+            Car newCar;
+            tollBooth.push_back(newCar);
+            cout << "Operation: Car joined toll; [" << newCar.getYear() << " " << newCar.getMake();
+            cout<< " (" <<  newCar.getTransponder() << ")]"<< endl;
+        }
+        else if (oper > 45) {
+            cout << "Operation: Car paid toll; [" << tollBooth.at(0).getYear() << " " << tollBooth.at(0).getMake();
+            cout<< " (" <<  tollBooth.at(0).getTransponder() << ")]"<< endl;
+            tollBooth.pop_front();
+        }
     
-    cout << "Updated deque:" << endl;
-    for (auto i = 0; i < tollBooth.size(); i++) {    // Outputs the inital queue
-        cout<< "[" << tollBooth.at(i).getYear() << " " << tollBooth.at(i).getMake();
-        cout<< " (" <<  tollBooth.at(i).getTransponder() << ")]"<< endl;
+        if (tollBooth.empty() == false) {
+            cout << "Updated Queue:" << endl;
+            for (auto i = 0; i < tollBooth.size(); i++) {
+                cout << setw(WIDTH) << "[" << tollBooth.at(i).getYear() << " " << tollBooth.at(i).getMake();
+                cout << " (" <<  tollBooth.at(i).getTransponder() << ")]"<< endl;
+            }
+        }
+        cout << "---------\n";
+        count++;
     }
-    cout << "---------\n";
+    cout << "[TOLL BOOTH COMPLETE]" << endl;
+   
     return 0;
+}
+
+int rand_num() {
+    random_device randNum;
+    uniform_int_distribution<int>range(1, 100);
+    int num = range(randNum);
+    
+    return num;
 }
